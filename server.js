f5de5680efdb5ba1d8def31b247c55e9577830f0
Mason Hargrove <maseh87@gmail.com>
2015-01-07 17:42:14 -0800

    added route

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 
 
+app.get('data', function(req, res, next) {
+  res.send('Hello World');
+});
+
 
 console.log(app, 'app');
 app.listen(process.env.PORT);
